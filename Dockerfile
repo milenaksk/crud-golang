ba1ee941e471719eb24412ee37ab921def663f6a
MILENA CASTOR <milena.castor@paycredex.com.br>
2024-07-25 17:11:45 -0300

    configurando bd

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ RUN apk update && apk add --no-cache git
 WORKDIR /app
 
 COPY go.mod ./
-COPY go.sum ./
 
 RUN go mod download
 
--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "net/http"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+type User struct { //entidade e atributos
+    Id int 
+    Name  string
+    Email string 
+    Age int
+}
+
+//métodos
 
 func Read(w http.ResponseWriter, r *http.Request) {
     // Sua implementação aqui
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
-    // Sua implementação aqui
+    if r.Method != "POST" {
+        http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed) //405 não permitido
+        return
+    }
+
+    u := User{}
+    err := json.NewDecoder(r.Body).Decode(&u)
+    if err != nil {
+        fmt.Println("server failed to handle ", err)
+        return
+    }
+
+    _,err = db.Exec("INSERT INTO users (name, email, age) VALUES ($1, $2, $3)", u.Name, u.Email, u.Age)
+    if err != nil {
+        fmt.Println("server failed to handle ", err)
+        return
+    }
+
+    w.WriteHeader(http.StatusCreated)
 }
 
 var db *sql.DB
